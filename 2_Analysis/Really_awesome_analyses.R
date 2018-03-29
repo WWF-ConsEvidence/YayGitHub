3482d46474beebe3a64e5faa0a6f0be5a64e839f
Claborn <Kelly.Claborn@wwfus.org>
2018-03-28 21:55:45 -0400

    All files staged, and existing directory ready to be added to YayGitHub repo.

---
+++ 2_Analysis/Really_awesome_analyses.R
@@ -0,0 +1,12 @@
+
+# This is where I've already done all of my super cutting edge analyses
+
+
+library(ggplot2)
+
+
+data <- data.frame(id=c(1,2,3,4),
+                   type.plant=c("pine.tree","rose.bush","sunflower","vine"),
+                   height=c(30,1,2,10))
+
+
